76b6e5a7660b88a8cddd76232eab1604bd17eccc
laozefeng <laozf@77ircloud.com>
2021-11-02 15:41:07 +0800

    feat: 更新配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,22 @@
-
 module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
-    "prettier"
+    'prettier',
   ],
-  plugins: ['@typescript-eslint',"babel-eslint",'prettier',],
+  plugins: ['@typescript-eslint', 'prettier'],
   env: {
     browser: true,
     es6: true,
     jest: true,
-  }, 
-  settings: {             
-    "react": {
-        "pragma": "React",
-        "version": "detect"
-    }
-  }, 
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -45,42 +43,12 @@ module.exports = {
     'no-plusplus': 0,
     'no-underscore-dangle': 0,
     'react/static-property-placement': 0,
-    'import/prefer-default-export':0,
+    'import/prefer-default-export': 0,
     'react/jsx-props-no-spreading': 1,
-    'import/order': [
-      1,
-      {
-        groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'object'],
-        // 按字母排序
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        pathGroups: [
-          {
-            pattern: '@/**',
-            group: 'parent',
-            position: 'before',
-          },
-          {
-            pattern: '@tarojs/**',
-            group: 'external',
-          },
-          {
-            pattern: './*.scss',
-            group: 'object',
-            position: 'after',
-          },
-        ],
-        pathGroupsExcludedImportTypes: [],
-        'newlines-between': 'always',
-      },
-    ],
+    'prettier/prettier': 1,
     '@typescript-eslint/no-empty-interface': 1,
     // 统一使用 LF
     'linebreak-style': ['error', 'unix'],
     'no-param-reassign': 0,
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars-experimental': 'error',
   },
 };
