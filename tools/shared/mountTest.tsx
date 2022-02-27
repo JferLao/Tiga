dbb5bb0aed21528244c8408a03eed0942a31b097
laozefeng <laozf@77ircloud.com>
2022-02-27 22:37:49 +0800

    chore: 测试lint

--- tools/shared/mountTest.tsx
+++ tools/shared/mountTest.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { mount } from 'enzyme'
+// eslint-disable-next-line jest/no-export
 export default function mountTest(Component: React.ComponentType): void {
   describe(`mount and unmount`, () => {
     it(`component could be updated and unmounted without errors`, () => {
